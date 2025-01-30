16f9a294daaaff3a605e4c48bc902c116bb31545
Dinis Matos <dinismatos2002@tecnico.ulisboa.pt>
2025-01-30 00:34:24 +0000

    Zonotope plot2d implemented

--- Reachability Methods/Zonotopes/zonotope.py
+++ Reachability Methods/Zonotopes/zonotope.py
@@ -1,5 +1,7 @@
 import numpy as np
 from operations import *
+from scipy.spatial import ConvexHull
+import matplotlib.pyplot as plt
 
 class Zonotope:
     def __init__(self, H, p):
@@ -35,5 +37,40 @@ class Zonotope:
     
     def volume(self):
         return ZonotopeVolume(self)
+    
+    def plot(self):
+        if self.H.shape[0] == 2:
+            return self.plot2D(self)
+        elif self.H.shape[0] == 3:
+            pass
+        else:
+            raise NotImplementedError("Plotting for dimensions higher than 3 is not supported.")
+            
 
+    def plot2D(self):
+        m, n = self.H.shape
 
+        m = self.H.shape[1]  # Number of generators
+        combinations = np.array(np.meshgrid(*[[-1, 1]] * m)).T.reshape(-1, m)
+
+        # Compute the points of the zonotope
+        points = combinations @ self.H.T + self.p # Matrix multiplication: c * G^T
+
+        # Compute the convex hull of the points
+        hull = ConvexHull(points)
+
+        # Plot the zonotope
+        plt.figure()
+        plt.fill(points[hull.vertices, 0], points[hull.vertices, 1], edgecolor='k', alpha=0.5, label="Zonotope")
+
+        # Plot the generators (optional)
+        for i in range(n):
+            plt.arrow(0, 0, self.H[0, i], self.H[1, i], head_width=0.1, head_length=0.1, fc='r', ec='r', label=f'Generator {i+1}' if i == 0 else "")
+
+        plt.title("2D Zonotope")
+        plt.xlabel("X-axis")
+        plt.ylabel("Y-axis")
+        plt.grid(True)
+        plt.legend()
+        plt.axis('equal')  # Ensure equal scaling
+        plt.show()
